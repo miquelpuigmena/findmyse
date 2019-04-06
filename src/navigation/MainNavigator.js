f8df64f7279f052dee5ba1787c064b6d13cc2a6d
Mar Vidal Segura <marvidalsegura@gmail.com>
2019-04-06 09:10:26 +0200

    afegir ranking i result al main navigator

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -6,12 +6,17 @@ import Story from '../components/story/Story';
 import Chat from '../components/chat/Chat';
 import Loading from '../components/loading/Loading';
 import Start from '../components/loading/Start';
+import Ranking from '../components/ranking/Ranking';
+import Result from '../components/result/Result';
 
 const HomeStack = createStackNavigator({
   Loading: Chat,
   Start: Start,
   Story: Story,
-  Chat: Chat
+  Chat: Chat,
+  Ranking: Ranking,
+  Result: Result
+
 }, {headerMode: 'none'});
 
 export default HomeStack;
