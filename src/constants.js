db5725550e587f61de3de7ba77cd1b1effe0a7a4
Carlos Rodríguez <carlos@goin.app>
2019-04-06 09:04:38 +0200

    CARDS

---
+++ src/constants.js
@@ -0,0 +1,110 @@
+const CARDS = [
+    {
+        id: 0,
+        text: "Expected monthly expenses",
+        type: "bar",
+        rightValue: "0",
+        leftValue: "30.000kr"
+    },
+    {
+        id: 1,
+        text: "Do you travel frequently?",
+        type: "choose",
+        rightValue: "Yes",
+        leftValue: "No"
+    },
+    {
+        id: 2,
+        text: "Which weather do you prefer?",
+        type: "choose",
+        rightValue: "Warm",
+        leftValue: "Cold"
+    },
+    {
+        id: 4,
+        text: "Would you work while studying?",
+        type: "choose",
+        rightValue: "yes",
+        leftValue: "no"
+    },
+    {
+        id: 5,
+        text: "Which topic do you like most",
+        type: "choose",
+        rightValue: "Humanities",
+        leftValue: "Sciences"
+    },
+    {
+        id: 6,
+        text: "Favourite speciality:",
+        type: "choose",
+        rightValue: "Natural sciences",
+        leftValue: "Engineering"
+    },
+    {
+        id: 7,
+        text: "Favourite speciality:",
+        type: "choose",
+        rightValue: "Social sciences",
+        leftValue: "Languages"
+    },
+    {
+        id: 8,
+        text: "Favourite speciality:",
+        type: "choose",
+        rightValue: "Business",
+        leftValue: "Law"
+    },
+    {
+        id: 9,
+        text: "About population...",
+        type: "bar",
+        rightValue: "Less is more",
+        leftValue: "The more the merrier"
+    },
+    {
+        id: 10,
+        text: "Rather to see through the window",
+        type: "choose",
+        rightValue: "Skyline",
+        leftValue: "Birds"
+    },
+    {
+        id: 11,
+        text: "Which landscape do you prefer?",
+        type: "choose",
+        rightValue: "Water",
+        leftValue: "Land"
+    },
+    {
+        id: 12,
+        text: "Select your favourite activity:",
+        type: "choose",
+        rightValue: "Nightlife",
+        leftValue: "Cultural time"
+    },
+    {
+        id: 13,
+        text: "Would you rather swim in ...",
+        type: "choose",
+        rightValue: "The sea",
+        leftValue: "A lake"
+    },
+    {
+        id: 14,
+        text: "What kind of people do you want to be surrounded with?",
+        type: "choose",
+        rightValue: "Young student community",
+        leftValue: "Big metropolitan city"
+    },
+    {
+        id: 15,
+        text: "Would you like to study in an international environment?",
+        type: "choose",
+        rightValue: "Yes",
+        leftValue: "No"
+    }
+
+]
+
+export {CARDS}
